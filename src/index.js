4963998b5dc9377869449458991ff59b4c4ad462
andrea <andreaepiayuepiayu.com>
2026-03-12 08:32:37 -0500

    feat: mi primer servidor Node.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('¡Mi primer servidor funciona!');
+});
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
